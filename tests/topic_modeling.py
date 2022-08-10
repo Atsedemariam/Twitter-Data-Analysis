4cd4bea2606195cb306d9a9aa66e3c3a6ac8613d
Atsedemariam <latseden@gmail.com>
2022-08-10 22:53:09 +0300

    spacing correction

--- tests/topic_modeling.py
+++ tests/topic_modeling.py
@@ -4,7 +4,7 @@ from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import * 
-import numpy as np
+import numpy as np 
 
 np.random.seed(2018)
 import nltk
