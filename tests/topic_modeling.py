06cd9dbf6c4145ea2eb054b6ffe0787bd8463548
Atsedemariam <latseden@gmail.com>
2022-08-10 22:45:08 +0300

    topic modeling using genism

--- tests/topic_modeling.py
+++ tests/topic_modeling.py
@@ -3,7 +3,7 @@ import gensim
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
-from nltk.stem.porter import *
+from nltk.stem.porter import * 
 import numpy as np
 
 np.random.seed(2018)
